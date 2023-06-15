839cd279e6012d7b962ff823769aaf5b67abff84
altermajor <118938444+Majoralter@users.noreply.github.com>
2023-06-15 21:33:37 +0100

    feat: create new section

--- src/desktop/Main/Main.svelte
+++ src/desktop/Main/Main.svelte
@@ -2,10 +2,12 @@
 import Hero from "./Hero.svelte";
 import About from "./About.svelte";
 import Work from "./Work.svelte";
+import Testimonials from "./Testimonials.svelte";
 </script>
 
 <main>
 <Hero />
 <About />
 <Work />
+<Testimonials />
 </main>
