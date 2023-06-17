76c318e0cf8bc957b7aee4ab8f0de236b3669c32
altermajor <118938444+Majoralter@users.noreply.github.com>
2023-06-17 20:03:36 +0100

    feat: add footer

--- src/App.svelte
+++ src/App.svelte
@@ -3,19 +3,10 @@
   import Nav from "./desktop/Nav.svelte";
   import Main from "./desktop/Main/Main.svelte";
   import Loading from "./desktop/Loading.svelte";
+  import Footer from "./desktop/Footer.svelte";
 </script>
 
 <Loading />
 <Nav />
 <Main />
-
-<!-- <section>
-
-</section>
-
-<style>
-  section{
-    height: 100vh;
-    background-color: orangered;
-  }
-</style> -->
+<Footer />
